b2fda2dd5f91f8127eb9467b61c401f4c00aa3b3
Mukesh kumar <91496440+mukesh0327@users.noreply.github.com>
2024-09-29 21:35:49 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Define a route for the root URL ('/')
+app.get('/', (req, res) => {
+    res.send('Hello, World! I am Mukesh!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
