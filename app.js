073ac11393a4c662efb3456374a9d31bb8b4495a
Mukesh kumar <91496440+mukesh0327@users.noreply.github.com>
2024-09-29 23:09:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;  // Make sure this is the port you intend to use
 
-// Define a route for the root URL ('/')
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
