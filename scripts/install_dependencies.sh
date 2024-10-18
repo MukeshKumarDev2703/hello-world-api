e81a29aabcd479e6fce368fa17db59285ad0a886
Simranpreet Singh <91496440+simranpreet0327@users.noreply.github.com>
2024-10-18 21:15:22 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,7 +2,7 @@
 git  clone https://github.com/simranpreet0327/hello-world-api.git
 cd /home/ec2-user/hello-world-api
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo yum -y nodejs
 
 sudo npm install
 sudo npm install express
