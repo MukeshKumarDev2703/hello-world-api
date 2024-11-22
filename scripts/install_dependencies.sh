d5be30fa46e94b9b32be7066abb51c83d38c498f
Mukesh Kumar <91496440+MukeshKumarDev2703@users.noreply.github.com>
2024-11-22 21:32:01 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-git  clone https://github.com/simranpreet0327/hello-world-api.git
-cd /home/ec2-user/hello-world-api
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-sudo yum install -y nodejs
-
-sudo npm install
-sudo npm install express
+cd /var/www/hello-world-api
+npm install
