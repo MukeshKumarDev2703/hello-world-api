b0f91dc06db7e73d942ab45e916849310e2dc47b
Mukesh Kumar <91496440+MukeshKumarDev2703@users.noreply.github.com>
2024-11-22 22:21:25 +0530

    Create chmod_scripts.sh

---
+++ scripts/chmod_scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Making install_dependencies.sh and start_server.sh executable
+chmod +x /var/www/hello-world-api/scripts/install_dependencies.sh
+chmod +x /var/www/hello-world-api/scripts/start_server.sh
