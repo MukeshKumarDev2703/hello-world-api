473c71f639a212549c21aa028e18d992449b8881
Mukesh Kumar <91496440+MukeshKumarDev2703@users.noreply.github.com>
2024-11-23 13:21:39 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
 # Navigate to the application directory
-cd /var/www/hello-world-api || { 
-  echo "Error: Directory /var/www/hello-world-api does not exist."; 
-  exit 1; 
-}
-
-# Check if package.json exists
-if [ ! -f package.json ]; then
-  echo "Error: package.json not found in /var/www/hello-world-api"
-  exit 1
-fi
+cd /var/www/hello-world-api
 
 # Install dependencies
 echo "Installing dependencies..."
 npm install
-
-if [ $? -eq 0 ]; then
-  echo "Dependencies installed successfully."
-else
-  echo "Error: Failed to install dependencies."
-  exit 1
-fi
